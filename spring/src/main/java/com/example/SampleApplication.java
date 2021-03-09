254c443f2904c7fea84c69395ca2ceabd9103db6
GowthamanM-Neo <gowthaman@iamneo.ai>
2021-03-09 18:33:17 +0530

    pom

--- spring/src/main/java/com/example/SampleApplication.java
+++ spring/src/main/java/com/example/SampleApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class SampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(SampleApplication.class, args);
 	}
 
 }
