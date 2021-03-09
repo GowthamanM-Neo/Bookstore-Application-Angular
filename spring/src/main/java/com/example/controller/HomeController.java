a4411d7e2c25d1d62d5088c2bd9b046e398d7b5d
GowthamanM-Neo <gowthaman@iamneo.ai>
2021-03-09 19:35:49 +0530

    Final Submit

--- spring/src/main/java/com/example/controller/HomeController.java
+++ spring/src/main/java/com/example/controller/HomeController.java
@@ -1,14 +1,13 @@
-package com.example;
+package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SampleController {
-	
+public class HomeController {
+
 	@GetMapping("/")
-	public String val() {
-		return "...working ...";
+	public String testApi() {
+		return "Api is working!";
 	}
-	
 }
